a521e5b5d92045e349eb044ca4254da8762a44ab
Juan De Dios <mirandajohn634@gmail.com>
2025-01-04 08:39:53 -0500

    se incluyen nuevas rutas en main.py

--- main.py
+++ main.py
@@ -1,23 +1,23 @@
-"""Main"""
+"""main"""
 
 from fastapi import FastAPI # type: ignore
-from routes import products, users
+from routes import products, users, basic_auth_users
 from fastapi.staticfiles import StaticFiles # type: ignore
 
 app = FastAPI()
 
+# Routers
+app.include_router(products.router)
+app.include_router(users.router)
+app.include_router(basic_auth_users.router)
+
+# Static files
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
 async def root():
-    """Def Root"""
-    return {"message": "Hello World"}
+    return "Hola FastAPI!"
 
 @app.get("/url")
 async def url():
-    """Def URL"""
     return {"url": "https://mouredev.com/python"}
-
-#Routes
-app.include_router(products.router)
-app.include_router(users.router)
-# Static Files
-app.mount("/static", StaticFiles(directory="static"), name="static")
